7390ceab7265f29e72db8374318115a853960c0d
David Knezić <davidknezic@gmail.com>
2020-04-06 10:25:57 +0200

    Add tests

---
+++ hooks/yaniv.test.ts
@@ -0,0 +1,31 @@
+/* eslint-env jest */
+
+import { renderHook, act } from '@testing-library/react-hooks'
+import { useYaniv } from './yaniv'
+
+describe('yaniv hook', () => {
+  beforeEach(() => {
+  })
+
+  it('should discard and draw card from stack', () => {
+    const { result } = renderHook(() => useYaniv())
+
+    act(() => result.current.setUp([['PK']], ['J1']))
+    act(() => result.current.discardAndDraw(0, ['PK'], 'J1'))
+
+    expect(result.current.hands).toStrictEqual([['J1']])
+    expect(result.current.stack).toStrictEqual([])
+  })
+
+  it('should discard and draw card from pile', () => {
+    const { result } = renderHook(() => useYaniv())
+
+    act(() => result.current.setUp([['PK']], ['J1']))
+    act(() => result.current.turnUp())
+    act(() => result.current.discardAndDraw(0, ['PK'], 'J1'))
+
+    expect(result.current.hands).toStrictEqual([['J1']])
+    expect(result.current.stack).toStrictEqual([])
+    expect(result.current.pile).toStrictEqual([['PK']])
+  })
+})
