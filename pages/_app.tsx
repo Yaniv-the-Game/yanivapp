47a8abf877f975e9ce430d15ace8f42e6d03c8f7
Pascal Reichmuth <pascal.reichmuth@gmail.com>
2020-04-18 21:37:16 +0800

    Fix Metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-<link href="https://fonts.googleapis.com/css2?family=Mali:wght@300;700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Mali:wght@300;700&display=swap" rel="stylesheet"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <meta name="description" content="The worlds best game now on your mobile device"/>
+        <meta name="copyright"content="2020 Dave & Pascal"/>
+        <meta name="language" content="EN"/>
       </Head>
       <Component {...pageProps} />
       <style jsx global>{`
